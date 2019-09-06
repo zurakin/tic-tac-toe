ab1abdc421147e8fb50680cc902de298aef8f20b
ILYASS AZIRAR <52070629+zurakin@users.noreply.github.com>
2019-09-07 00:10:26 +0100

    finally completed the game updated readme

--- game.py
+++ game.py
@@ -9,17 +9,17 @@ class Game:
 
     def list_pw(self):
         global strikes
-        return [ [ self.table[int(pos)] for pos in L ] for L in strikes]
+        return [ [ int(pos) for pos in L ] for L in strikes]
 
     def check_winner(self):
         for p in self.list_pw():
-            if all([ i == 'X' for i in p]):
+            if all([ self.table[i] == 'X' for i in p]):
                 self.winner='X'
-                return 'X'
+                return tuple(p)
         for p in self.list_pw():
-            if all([ i == 'O' for i in p]):
+            if all([ self.table[i] == 'O' for i in p]):
                 self.winner='O'
-                return "O"
+                return tuple(p)
         if self.winner==None and all([self.table[i]!=None for i in range(9)]):
             self.winner=='draw'
-        return None
+        return
--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import ImageTk
 
+
 def convert(pos):
     return pos[1]*3 + pos[0]
 
@@ -23,7 +24,21 @@ class Window:
         ##add images for x and o
         self.x_im = ImageTk.PhotoImage(file = r"media\x.png")
         self.o_im = ImageTk.PhotoImage(file = r"media\o.png")
+        self.hbar = ImageTk.PhotoImage(file = r"media\hbar.png")
+        self.vbar = ImageTk.PhotoImage(file = r"media\vbar.png")
+        self.diaglr = ImageTk.PhotoImage(file = r"media\diaglr.png")
+        self.diagrl = ImageTk.PhotoImage(file = r"media\diagrl.png")
         self.images = []
+        self.lines = {
+        (0,1,2):(self.hbar,(0,0)),
+        (3,4,5):(self.hbar,(0,200)),
+        (6,7,8):(self.hbar,(0,400)),
+        (0,3,6):(self.vbar,(0,0)),
+        (1,4,7):(self.vbar,(200,0)),
+        (2,5,8):(self.vbar,(400,0)),
+        (0,4,8):(self.diaglr,(0,0)),
+        (2,4,6):(self.diagrl,(0,0))
+        }
 
     def click(self,event):
         if self.game.winner != None:
@@ -40,7 +55,15 @@ class Window:
             self.images.append(self.canvas.create_image(int(event.x/200)*200,
             int(event.y/200)*200, image = self.o_im,anchor = NW ))
             self.game.turn = 'X'
+
         self.window.update()
-        self.game.check_winner()
+        winning_line = self.game.check_winner()
+        if winning_line!= None:
+            self.images.append(self.canvas.create_image(self.lines[winning_line][1][0],
+            self.lines[winning_line][1][1],
+            image = self.lines[winning_line][0],
+            anchor = NW))
+
+
         if self.game.winner != None:
             print('the winner is {}\nPress return to restart'.format(self.game.winner))
